8c903b8239d24ad6c3e079fa9cd904df1cb882e7
Aditya Surana <to.adityasurana@gmail.com>
2022-12-28 00:38:21 +0530

    Add files via upload

---
+++ amazon-clone-frontend/src/Components/Checkout.js
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import "./Checkout.css";
+import Grid from '@material-ui/core/Grid';
+import CheckoutItems from './CheckoutItems';
+import { CartContext } from '../CartContext';
+
+function Checkout(props) {
+    const { item, size, increment } = useContext(CartContext);
+
+    const cartValue = function(){
+        let price = 0;
+        for(let i=0; i < item.length; i++){
+            price += parseInt(item[i].price);
+        }
+        return price;
+    }
+
+    return (
+        <div className="checkout__body">
+             <Grid container >
+                 <Grid item={10}>
+                    <div className="checkout__container">
+                        <div style={{fontSize: "30px" , fontWeight: "500" , padding : "20px 0px 0px 20px"}}>Shopping Cart</div>
+                        <div>
+                            { 
+                                item.length >0 ?
+                                item.map( (value) => (
+                                    <CheckoutItems definition={value} />
+                                ))
+                                : <div style={{height: "100vh", margin: "30px" }}> Please buy something</div>
+                            }
+                        </div>
+                    </div>
+                 </Grid>
+                 <Grid item={2}>
+                    <div style={{ width: "300px", height:"200px", padding: "20px", marginTop: "25px", backgroundColor: "white"}}>
+                        <div style={{ fontSize: "26px"}}>Subtotal ( {size} items): <strong>{ cartValue() }</strong></div>
+                        <div style={{paddingTop : "25px "}}>
+                            <button className="placeorder__button" >Proceed to Buy</button>
+                        </div>
+                    </div>
+                 </Grid>
+             </Grid>
+        </div>
+    );
+}
+
+export default Checkout;
---
+++ amazon-clone-frontend/src/Components/CheckoutItems.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Checkout.css";
+
+function CheckoutItems(props) {
+    return (
+        <div>
+            <div style={{ border: "1px solid #E7E7E7", width: "95%", display:"flex", height: "250px", margin: "25px"}}>
+                <div style={{margin: "25px"}}>
+                    <img height="200px" src={props.definition.image} />
+                </div>
+                <div style={{ marginTop: "20px"}}>
+                    <div style={{fontSize: "20px"}} className="textgap">{props.definition.name}</div>
+                    <div style={{ fontWeight: "bold"}} className="textgap">{props.definition.price}</div>
+                    <div className="textgap">{props.definition.status}</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CheckoutItems;
