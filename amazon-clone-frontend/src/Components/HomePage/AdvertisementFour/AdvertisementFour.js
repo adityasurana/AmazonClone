8b46bc82b2c9b505c6305f1a9ee12789e7eb6e20
Aditya Surana <to.adityasurana@gmail.com>
2022-12-29 01:11:00 +0530

    Add files via upload

---
+++ amazon-clone-frontend/src/Components/HomePage/AdvertisementFour/AdvertisementFour.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./AdvertisementFour.css"
+
+function AdvertisementFour(props) {
+    return (
+        <div className='AdvertisementOne__main'>
+            <div className='AdvertisementOne__header'>
+                Up to 70% off | Electronics clearance store
+            </div>
+            <div className='AdvertisementOne__body'>
+                <img src="https://ik.imagekit.io/amazonClone123/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1670262839009" className='ad_fourImage' />
+                <img src="https://ik.imagekit.io/amazonClone123/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1670262839009" className='ad_fourImage' />
+                <img src="https://ik.imagekit.io/amazonClone123/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1670262839009" className='ad_fourImage' />
+                <img src="https://ik.imagekit.io/amazonClone123/amazon-image/V238940049_IN_PC_BAU_Edit_Creation_Laptops2X._SY608_CB667377204_.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1670262839009" className='ad_fourImage' />
+            </div>
+            <div className='AdvertisementOne__footer'>
+                See more
+            </div>
+        </div>
+    )
+}
+
+export default AdvertisementFour;
