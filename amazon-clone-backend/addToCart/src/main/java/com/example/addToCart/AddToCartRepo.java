7e4a1f76e85f2a1c16147b1a2bc603d0a382fb92
Aditya Surana <to.adityasurana@gmail.com>
2022-12-30 00:17:25 +0530

    Add files via upload

---
+++ amazon-clone-backend/addToCart/src/main/java/com/example/addToCart/AddToCartRepo.java
@@ -0,0 +1,16 @@
+package com.example.addToCart.Repository;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.addToCart.Entity.Cart;
+
+@Repository
+public interface AddToCartRepo extends CrudRepository<Cart, Long> {
+	Cart save(Cart cart);
+	void delete(Cart cart);
+	ArrayList<Cart> findByUserId(UUID userId);
+}
---
+++ amazon-clone-backend/addToCart/src/main/java/com/example/addToCart/AddToCartService.java
@@ -0,0 +1,49 @@
+package com.example.addToCart.Service;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.addToCart.Entity.Cart;
+import com.example.addToCart.Entity.CartDetails;
+import com.example.addToCart.Entity.Product;
+import com.example.addToCart.Repository.AddToCartRepo;
+
+@Service
+public class AddToCartService {
+	
+	@Autowired
+	AddToCartRepo addToCartRepo;
+	
+	@Autowired
+	ProductDetailsProxy productDetailsProxy;
+	
+	public void addItemToCartService(Cart cart) {
+		addToCartRepo.save(cart);
+	}
+	
+	public void removeItemFromCartService(Cart cart) {
+		addToCartRepo.delete(cart);
+	}
+	
+	public CartDetails displayAllProductsInCart(UUID userId) {
+		
+		CartDetails cartDetails = new CartDetails();
+		cartDetails.setUserId(userId);
+		
+		ArrayList<Cart> cartList = addToCartRepo.findByUserId(userId);
+		System.out.println(cartList);
+		ArrayList<Product> productList = new ArrayList<Product>();
+		
+		for(int i=0; i<cartList.size(); i++) {
+			Product product = productDetailsProxy.getProductById(cartList.get(i).getProductId());
+			productList.add(product);
+		}
+		
+		cartDetails.setList(productList);
+		return cartDetails;
+	}
+
+}
