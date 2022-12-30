f70e3260e2be9105e14c44fd8a6b6bd180d47728
Aditya Surana <to.adityasurana@gmail.com>
2022-12-31 00:11:54 +0530

    Add files via upload

---
+++ amazon-clone-backend/productdetails/ProductdetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductdetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
